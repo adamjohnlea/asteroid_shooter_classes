384ac55a949be65f70d1dbb2c64c2a767cdfc700
adamjohnlea <imnogeekdptcom@me.com>
2022-12-23 15:21:45 -0700

    add asteroid rotation and random size

--- app.py
+++ app.py
@@ -60,16 +60,29 @@ class Laser(pygame.sprite.Sprite):
 class Asteroid(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('./graphics/meteor.png').convert_alpha()
+        asteroid_surf = pygame.image.load('./graphics/meteor.png').convert_alpha()
+        asteroid_size = pygame.math.Vector2(asteroid_surf.get_size()) * uniform(0.5, 1.5)
+        self.scaled_surf = pygame.transform.scale(asteroid_surf, asteroid_size)
+        self.image = self.scaled_surf
         self.rect = self.image.get_rect(center=pos)
 
         self.pos = pygame.math.Vector2(self.rect.topleft)
         self.direction = pygame.math.Vector2(uniform(-0.5, 0.5), 1)
         self.speed = randint(400, 600)
 
+        self.rotation = 0
+        self.rotation_speed = randint(20, 50)
+
+    def rotate(self):
+        self.rotation += self.rotation_speed * dt
+        rotated_surf = pygame.transform.rotate(self.scaled_surf, self.rotation)
+        self.image = rotated_surf
+        self.rect = self.image.get_rect(center=self.rect.center)
+
     def update(self):
         self.pos += self.direction * self.speed * dt
         self.rect.topleft = (round(self.pos.x), round(self.pos.y))
+        self.rotate()
 
 
 class Text:
